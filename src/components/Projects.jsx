9853f4413c4be23720573a76c927dc8e691f2901
Dinesh Prajapati <dineshchauhan416pd@gmail.com>
2024-05-04 10:13:18 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -145,13 +145,13 @@ project_link
             >
               <img
                 src={link}
-                alt='source code'
+                alt='project link'
                 className='w-1/2 h-1/2 object-contain'
               />
             </div>
             </div>
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div className='absolute inset-0 flex justify-end m-3'>
             <div
               onClick={() => window.open(source_code_link, "_blank")}
               className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
